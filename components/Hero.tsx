aed353b6d8fdf4393d8205a54f529981f0d01a36
Max Programming <ui.sabuwala@gmail.com>
2021-09-30 17:32:12 +0530

    🎉 Initial commit

---
+++ components/Hero.tsx
@@ -0,0 +1,39 @@
+import Link from 'next/link';
+
+const languages = [
+  'JavaScript',
+  'Python',
+  'HTML',
+  'Java',
+  'PHP',
+  'C++',
+  'TypeScript',
+  'Go',
+  'CSS',
+  'C',
+];
+
+export default function Hero() {
+  return (
+    <div className='min-h-screen hero'>
+      <div className='text-center hero-content'>
+        <div className='max-w-md'>
+          <h1 className='mb-5 text-5xl font-bold'>Select your language</h1>
+          <p className='mb-5'>
+            Select the programming language you would like to find repositories
+            for.
+          </p>
+          {languages.map(language => (
+            <Link key={language} href={`/repos/${language.toLowerCase()}`}>
+              <a>
+                <button className='m-2 btn btn-lg btn-warning'>
+                  {language}
+                </button>
+              </a>
+            </Link>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ pages/repos/[language].tsx
@@ -0,0 +1,51 @@
+import { GetServerSideProps } from 'next';
+import Card from '../../components/Card';
+import Header from '../../components/Header';
+
+interface Props {
+  languageName: string;
+  repos: any;
+}
+
+export default function Language({ repos, languageName }: Props) {
+  return (
+    <>
+      <Header />
+      <div className='container mx-auto'>
+        <div className='min-h-screen pt-5'>
+          <div className='text-center'>
+            <div className='w-5/6 max-w-md mx-auto'>
+              <h1 className='mb-5 text-5xl font-bold'>
+                {repos.total_count} repositories for{' '}
+                <span className='font-mono underline text-warning'>
+                  {languageName.toUpperCase()}
+                </span>
+              </h1>
+            </div>
+          </div>
+          <div className='grid grid-cols-3 gap-4'>
+            {repos.items.map(repo => (
+              <Card key={repo.id} repo={repo} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export const getServerSideProps: GetServerSideProps = async ctx => {
+  const languageName = ctx.params?.language;
+  const apiUrl = `https://api.github.com/search/repositories?q=topic%3Ahacktoberfest+language%3A${languageName}`;
+  const res = await fetch(apiUrl, {
+    headers: { Accept: 'application/vnd.github.mercy-preview+json' },
+  });
+  const repos = await res.json();
+  console.log(repos.items);
+  return {
+    props: {
+      repos,
+      languageName,
+    },
+  };
+};
