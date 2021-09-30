4fac7dc2ea38d5d80a2f0bb5e359184d4d4c8e74
janaSunrise <warriordefenderz@gmail.com>
2021-09-30 18:15:41 +0530

    🎨 Add SEO.

---
+++ next-seo.config.ts
@@ -0,0 +1,34 @@
+const config = {
+  title: 'Hacktoberfest Projects',
+  description: `Quickly and easily find projects to contribute to this Hacktoberfest 2021!`,
+  additionalMetaTags: [
+    {
+      property: 'keywords',
+      content: 'hacktoberfest, open-source, contribution, oss'
+    },
+    {
+      name: 'theme-color',
+      content: '#1A202C'
+    },
+    {
+      name: 'color-scheme',
+      content: 'dark'
+    }
+  ],
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: 'https://hacktoberfest-projects.vercel.app',
+    title: 'Hacktoberfest projects',
+    description: `Quickly and easily find projects to contribute to this Hacktoberfest 2021!`,
+    site_name: 'Hacktoberfest projects'
+  },
+  twitter: {
+    creator: '@MaxProgramming1',
+    cardType: 'summary',
+    title: 'Hacktoberfest projects',
+    url: 'https://hacktoberfest-projects.vercel.app'
+  }
+};
+
+export default config;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,15 @@
 import type { AppProps } from 'next/app';
 import NextNProgress from 'nextjs-progressbar';
+import { DefaultSeo } from 'next-seo';
+
+import SEO from 'next-seo.config';
+
 import 'tailwindcss/tailwind.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <DefaultSeo {...SEO} />
       <NextNProgress color="yellow" options={{ showSpinner: false }} />
       <Component {...pageProps} />
     </>
