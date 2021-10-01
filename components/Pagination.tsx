b45c4879350a4326a763ba6d4f3658b15e828364
janaSunrise <warriordefenderz@gmail.com>
2021-10-01 12:32:51 +0530

    Add icons for the pagination.

--- components/Pagination.tsx
+++ components/Pagination.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import { MouseEventHandler } from 'react';
+import { BsArrowLeft, BsArrowRight } from 'react-icons/bs';
 
 interface Props {
   languageName: string;
@@ -8,12 +8,18 @@ interface Props {
 
 export default function Pagination({ languageName, page }: Props) {
   return (
-    <div className="items-center justify-center my-5 btn-group">
+    <div className="items-center justify-center my-6 btn-group">
       <Link href={`/repos/${languageName}?p=${page - 1}`}>
-        <a className="btn btn-outline btn-wide">Previous Page</a>
+        <a className="btn btn-outline btn-wide">
+          <BsArrowLeft />
+          <span className="ml-2">Previous Page</span>
+        </a>
       </Link>
       <Link href={`/repos/${languageName}?p=${page + 1}`}>
-        <a className="btn btn-outline btn-wide">Next Page</a>
+        <a className="btn btn-outline btn-wide">
+          <span className="mr-2">Next Page</span>
+          <BsArrowRight />
+        </a>
       </Link>
     </div>
   );
