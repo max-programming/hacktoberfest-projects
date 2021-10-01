9b4c025722b0a119ff7e1b3e8b0de0872ecb5ffd
Max Programming <ui.sabuwala@gmail.com>
2021-10-01 18:16:00 +0530

    🐛 Fix pagination + sort bug

--- components/Pagination.tsx
+++ components/Pagination.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import { BsArrowLeft, BsArrowRight } from 'react-icons/bs';
 
 interface Props {
@@ -7,15 +8,24 @@ interface Props {
 }
 
 const Pagination = ({ languageName, page }: Props) => {
+  const router = useRouter();
   return (
     <div className="items-center justify-center my-6 btn-group">
-      <Link href={`/repos/${languageName}?p=${page - 1}`}>
+      <Link
+        href={`/repos/${languageName}?p=${page - 1}&s=${router.query.s}&o=${
+          router.query.o
+        }`}
+      >
         <a className="btn btn-outline btn-wide">
           <BsArrowLeft />
           <span className="ml-2">Previous Page</span>
         </a>
       </Link>
-      <Link href={`/repos/${languageName}?p=${page + 1}`}>
+      <Link
+        href={`/repos/${languageName}?p=${page + 1}&s=${router.query.s}&o=${
+          router.query.o
+        }`}
+      >
         <a className="btn btn-outline btn-wide">
           <span className="mr-2">Next Page</span>
           <BsArrowRight />
