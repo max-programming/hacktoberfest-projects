2971f1b137cd0a06978c748faad3596d95a025bd
Your Name <avneeshagarwal0612@gmail.com>
2021-10-02 11:58:36 +0530

    removed extra animation

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -28,8 +28,6 @@ const Hero = () => {
                 <motion.button
                   initial={{ scale: 0.2 }}
                   animate={{ scale: 1 }}
-                  whileHover={{ scale: 1.1 }}
-                  whileTap={{ scale: 0.9 }}
                   transition={{ duration: 0.3 }}
                   className="m-2 bg-primary hover:bg-hero-button-hover border-0 btn btn-lg"
                 >
