93025224abd7fc6b0b7cb12f8fb0c4cc088cb383
hussamkhatib <hussamkhatib20@gmail.com>
2021-10-06 20:18:56 +0530

    add favicon's for various viewports

--- next-seo.config.ts
+++ next-seo.config.ts
@@ -13,6 +13,9 @@ const config = {
     {
       name: 'color-scheme',
       content: 'dark'
+    },{
+    name: 'msapplication-TileColor',
+    content: '#da532c'
     }
   ],
   openGraph: {
@@ -28,7 +31,33 @@ const config = {
     cardType: 'summary',
     title: 'Hacktoberfest projects',
     url: 'https://hacktoberfest-projects.vercel.app'
-  }
+  },
+  additionalLinkTags: [
+    {
+      rel: 'icon',
+      href: '/favicon.ico',
+    },
+    {
+      rel: 'icon',
+      href: '/favicon-32x32.ico',
+      sizes: '32x32',
+    },
+    {
+      rel: 'icon',
+      href: '/favicon-16x16.ico',
+      sizes: '16x16',
+    },
+    {
+      rel: 'apple-touch-icon',
+      href: '/apple-touch-icon.ico',
+      sizes: '76x76',
+    },
+    {
+      rel: 'mask-icon',
+      href: '/safari-pinned-tab.svg',
+      color: "#5bbad5",
+    },
+  ],
 };
 
 export default config;
