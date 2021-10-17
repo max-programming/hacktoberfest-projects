44f6aa916a1eeb7353ca87d3254bc7b2bd2d76e4
hussamkhatib <hussamkhatib20@gmail.com>
2021-10-17 21:47:10 +0530

    fix breaking changes in starStars ,endStars input

--- components/StarsFilter.tsx
+++ components/StarsFilter.tsx
@@ -1,44 +1,68 @@
 import { useRouter } from 'next/router';
-import { SubmitHandler, useForm } from 'react-hook-form';
+import { SubmitHandler, Controller, useForm } from 'react-hook-form';
 import { GoX } from 'react-icons/go';
 
 interface FormValues {
   startStars: number;
-  endStars: number;
+  endStars: number | string;
 }
 
 export default function StarsFilter() {
   const router = useRouter();
-  const { register, handleSubmit } = useForm<FormValues>();
+  const { handleSubmit, control, reset } = useForm<FormValues>();
+
   const onSubmit: SubmitHandler<FormValues> = ({ startStars, endStars }) => {
-    const query = endStars
-      ? {
-          startStars,
-          endStars,
-        }
-      : { startStars };
-    router.push({ query: { ...router.query, ...query } });
+    let query;
+    if (endStars < startStars) {
+      reset({ startStars, endStars: '' });
+      query = { startStars };
+      const { endStars, ...rest } = router.query;
+      router.push({ query: { ...rest, ...query } });
+    } else {
+      query = {
+        startStars,
+        endStars
+      };
+      router.push({ query: { ...router.query, ...query } });
+    }
   };
 
   return (
     <form
-      className='w-full m-2 mx-auto mb-4 lg:w-2/4 form-control'
+      className="w-full m-2 mx-auto mb-4 lg:w-2/4 form-control"
       onSubmit={handleSubmit(onSubmit)}
     >
-      <div className='flex gap-2'>
-        <input
-          placeholder='Start stars'
-          className='w-full pr-16 input input-primary input-bordered border-primary'
-          type='number'
-          {...register('startStars', { required: true })}
+      <div className="flex gap-2">
+        <Controller
+          name="startStars"
+          render={({ field }) => (
+            <input
+              type="number"
+              className="w-full pr-16 input input-primary input-bordered border-primary"
+              {...field}
+              onChange={e => {
+                field.onChange(parseInt(e.target.value, 10));
+              }}
+            />
+          )}
+          control={control}
         />
-        <input
-          placeholder='End stars'
-          className='w-full pr-16 input input-primary input-bordered border-primary'
-          type='number'
-          {...register('endStars')}
+        <Controller
+          name="endStars"
+          render={({ field }) => (
+            <input
+              type="number"
+              className="w-full pr-16 input input-primary input-bordered border-primary"
+              {...field}
+              onChange={e => {
+                field.onChange(parseInt(e.target.value, 10));
+              }}
+            />
+          )}
+          control={control}
         />
-        <button className='rounded-l-none btn btn-ghost' type='submit'>
+
+        <button className="rounded-l-none btn btn-ghost" type="submit">
           Search
         </button>
       </div>
