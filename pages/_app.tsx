df8fac26d19c0bc61e6b5abacc5625ed514ff752
Vidya S <vidyashivakumar221@gmail.com>
2021-10-29 19:21:11 +0530

    color change

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -25,7 +25,7 @@ function MyApp({ Component, pageProps, router }: AppProps) {
       }}
     >
       <DefaultSeo {...SEO} />
-      <NextNProgress color="#f54418" options={{ showSpinner: false }} />
+      <NextNProgress color="#f54318" options={{ showSpinner: false }} />
       <Component {...pageProps} />
     </motion.div>
   );
