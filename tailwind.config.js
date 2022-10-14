0f59ea19aca600b3f6a9536f9a2a8a5c91ca2cb7
Max Programming <ui.sabuwala@gmail.com>
2022-10-14 20:58:51 +0530

    🔧 Update Tailwind Configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,17 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}'
+  ],
   media: false, // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
-        "primary-1": '#ffd74d',
-        "primary-2": '#40ddff',
-        "primary-3": '#7c7fff',
-        "primary": '#7c7fff',
+        'primary-1': '#ffd74d',
+        'primary-2': '#40ddff',
+        'primary-3': '#7c7fff',
+        primary: '#7c7fff',
         accent: '#170f1e',
         'hero-button-hover': '#b53a25'
       }
