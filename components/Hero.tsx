bf33f9a72e0cbd5cd791993e2324c4fec2c1530c
Tejash Patel <pateltejash429@gmail.com>
2022-10-16 04:30:51 +0530

    solved the home page background color problem and changed the search stars button

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -18,7 +18,7 @@ const Hero = () => {
         backgroundSize: '80%'
       }}
     >
-      <div className="hero-overlay bg-opacity-60"></div>
+      <div className="hero-overlay w-0 bg-opacity-60"></div>
       <div className="text-center hero-content">
         <div className="max-w-md">
           <h1 className="mb-5 text-5xl font-bold">Select your language</h1>
