263fbdcb20b243a1eb27b41b8f881a860cd3b5e7
jpdias <jpdias@live.com.pt>
2022-10-21 21:18:49 +0100

    reset changes on Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,5 @@
-import Link from 'next/link';
 import Image from 'next/image';
+import Link from 'next/link';
 import { useRouter } from 'next/router';
 
 import { IoLogoGithub } from 'react-icons/io';
