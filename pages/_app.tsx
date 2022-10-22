f64330c8bb2153d458aa6201f943c68bc9403f8e
Usman Sabuwala (Max Programming) <51731966+max-programming@users.noreply.github.com>
2022-10-22 18:42:43 +0530

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import NextNProgress from 'nextjs-progressbar';
 import { DefaultSeo } from 'next-seo';
 import { motion } from 'framer-motion';
@@ -24,6 +25,9 @@ function MyApp({ Component, pageProps, router }: AppProps) {
         }
       }}
     >
+      <Head>
+        <script async defer data-website-id="2529a688-ee5c-4869-9f85-7bb8fa5b6518" src="https://insights.usman-s.me/umami.js"></script>
+      </Head>
       <DefaultSeo {...SEO} />
       <NextNProgress color="#ffd74d" options={{ showSpinner: false }} />
       <Component {...pageProps} />
