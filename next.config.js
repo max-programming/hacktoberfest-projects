f64f576f742e789ab8ff5225f63b1165fe39663d
Usman S. (Max Programming) <51731966+max-programming@users.noreply.github.com>
2023-03-17 17:00:23 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,4 +4,13 @@ module.exports = {
   images: {
     domains: ['avatars.githubusercontent.com', 'raw.githubusercontent.com'],
   },
+  async redirects() {
+    return [
+      {
+        source: '/github',
+        destination: 'https://github.com/max-programming/hacktoberfest-projects',
+        permanent: true,
+      },
+    ]
+  },
 };
