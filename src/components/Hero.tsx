a737289789dc1d3c86da3316536610c504401af5
Max Programming <ui.sabuwala@gmail.com>
2023-05-10 20:11:20 +0530

    Fix key error and add .vercel to gitignore

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -9,31 +9,31 @@ const { main: mainLanguages, others: otherLanguages } = languages;
 export default function Hero() {
   return (
     <div>
-      <div className="min-h-screen hero">
-        <div className="hero-overlay w-0 bg-opacity-60"></div>
-        <div className="text-center hero-content">
-          <div className="max-w-md">
-            <h1 className="mb-5 text-5xl font-bold">Select your language</h1>
-            <p className="mb-5">
+      <div className='min-h-screen hero'>
+        <div className='hero-overlay w-0 bg-opacity-60'></div>
+        <div className='text-center hero-content'>
+          <div className='max-w-md'>
+            <h1 className='mb-5 text-5xl font-bold'>Select your language</h1>
+            <p className='mb-5'>
               Select the programming language you would like to find
               repositories for.
             </p>
 
             {mainLanguages.map(language => (
-              <LanguageButton language={language} />
+              <LanguageButton key={language} language={language} />
             ))}
 
-            <div className="dropdown dropdown-top">
+            <div className='dropdown dropdown-top'>
               <div
                 tabIndex={0}
-                className="m-1 btn btn-lg btn-clip hover:bg-primary-2 hover:text-black umami--click--otherlangs-button"
+                className='m-1 btn btn-lg btn-clip hover:bg-primary-2 hover:text-black umami--click--otherlangs-button'
               >
                 Other languages
               </div>
 
               <ul
                 tabIndex={0}
-                className="h-64 p-2 overflow-y-scroll shadow menu dropdown-content bg-base-100 rounded-box w-60"
+                className='h-64 p-2 overflow-y-scroll shadow menu dropdown-content bg-base-100 rounded-box w-60'
               >
                 {otherLanguages.sort(sortByName).map(language => (
                   <li key={language}>
