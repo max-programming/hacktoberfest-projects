80f924f4f887253bbcee1288bc6220993cf1290e
Alone-Y154 <yashwanthkris154@gmail.com>
2023-09-26 00:04:30 +0530

    #79-Added the Icons for the Language buttons

--- components/LanguageButton.tsx
+++ components/LanguageButton.tsx
@@ -2,9 +2,13 @@ import Link from 'next/link';
 import Button from './Button';
 
 function LanguageButton({ language }: { language: string }) {
+  if (language == "HTML") {
+    language="HTML5"
+  }
+  const url = `https://cdn.simpleicons.org/${language.toLowerCase()}/default`;
   return (
     <Link href={`/repos/${language.toLowerCase()}`}>
-      <Button>{language}</Button>
+      <Button>{language} <img className='ml-1 h-5 w-5' src={url} /></Button>
     </Link>
   );
 }
