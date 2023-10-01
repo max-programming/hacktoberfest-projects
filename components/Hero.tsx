f5d21a184889fea3a913c365efb27a3153a5e0a0
malik-vishu <malik03ai@gmail.com>
2023-10-01 14:51:43 +0530

    color changes main page

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -22,10 +22,10 @@ function Hero() {
       <div className="hero-overlay w-0 bg-opacity-60"></div>
       <div className="text-center hero-content">
         <div className="max-w-md">
-          <h1 className="mb-5 text-neutral-700 text-5xl font-bold uppercase">
+          <h1 className="mb-5 text-2023-manga-3 text-5xl font-bold uppercase">
             Select your language
           </h1>
-          <p className="mb-5 text-neutral-700 font-semibold uppercase">
+          <p className="mb-5 text-2023-manga-3 font-semibold uppercase">
             Select the programming language you would like to find repositories
             for.
           </p>
@@ -57,7 +57,7 @@ function Hero() {
             onSubmit={handleSubmit}
           >
             <label className="label">
-              <span className="label-text text-neutral-700">Can&apos;t find your language?</span>
+              <span className="label-text text-2023-manga-3">Can&apos;t find your language?</span>
             </label>
             <div className="flex">
               <input
