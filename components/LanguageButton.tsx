46768344742d8b7b72ef9f9ea7dddcd65c869bae
Alone-Y154 <yashwanthkris154@gmail.com>
2023-10-03 21:31:10 +0530

    made required changes for the icons

--- components/LanguageButton.tsx
+++ components/LanguageButton.tsx
@@ -2,13 +2,23 @@ import Link from 'next/link';
 import Button from './Button';
 
 function LanguageButton({ language }: { language: string }) {
-  if (language == "HTML") {
-    language="HTML5"
+  let url;
+  if (language == 'HTML') {
+    language = 'HTML5';
+    url = `https://cdn.simpleicons.org/${language.toLowerCase()}/default`;
+  } else if (language == 'Java') {
+    url = 'https://img.icons8.com/color/48/java-coffee-cup-logo--v1.png';
+  } else if (language == 'Rust') {
+    url = `https://cdn.simpleicons.org/${language.toLowerCase()}/red`;
+  } else {
+    url = `https://cdn.simpleicons.org/${language.toLowerCase()}/default`;
   }
-  const url = `https://cdn.simpleicons.org/${language.toLowerCase()}/default`;
   return (
     <Link href={`/repos/${language.toLowerCase()}`}>
-      <Button> <img className='mr-1 h-5 w-5' src={url} alt={language}/> {language}</Button>
+      <Button>
+        {' '}
+        <img className="mr-1 h-5 w-5" src={url} alt={language} /> {language}
+      </Button>
     </Link>
   );
 }
