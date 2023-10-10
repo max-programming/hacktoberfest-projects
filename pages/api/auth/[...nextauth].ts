f97556509580e9db92cfbca0c6749e93cc8a4576
Usman S. (Max Programming) <ui.sabuwala@gmail.com>
2023-10-10 13:29:01 +0530

    🏷️ Fix type errors

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -3,12 +3,15 @@ import NextAuth, { type AuthOptions } from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
 
 export const authOptions = {
-  providers: [
-    GithubProvider({
-      clientId: env.GITHUB_ID,
-      clientSecret: env.GITHUB_SECRET
-    })
-  ]
+  providers:
+    env.GITHUB_ID && env.GITHUB_SECRET
+      ? [
+          GithubProvider({
+            clientId: env.GITHUB_ID,
+            clientSecret: env.GITHUB_SECRET
+          })
+        ]
+      : []
 } satisfies AuthOptions;
 
 export default NextAuth(authOptions);
