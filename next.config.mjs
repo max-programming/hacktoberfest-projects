b030e685e248043e0b3a0c6043840b41aae053b9
Pedro Pablo Pastene <pe.pastene@gmail.com>
2023-10-13 23:34:36 -0300

    Add the watch option for the development setup

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,14 @@ await import('./env.mjs');
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  swcMinify: true,
+  webpack: (config, context) => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300
+    }
+    return config
+  },
   async redirects() {
     return [
       {
