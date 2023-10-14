0083583db500e194bbac0085e4301b277ddba2de
Alone-Y154 <yashwanthkris154@gmail.com>
2023-10-14 20:45:13 +0530

    UI

--- components/LanguageButton.tsx
+++ components/LanguageButton.tsx
@@ -3,22 +3,18 @@ import Button from './Button';
 import Icons from './Icons';
 
 function LanguageButton({ language }: { language: string }) {
-
-  let lan:string = language.toLowerCase();
-  lan === "c++" ? lan = "cpp" : "";
+  let lan: string = language.toLowerCase();
+  lan === 'c++' ? (lan = 'cpp') : '';
   let icon = Icons[lan];
 
   return (
     <Link href={`/repos/${language.toLowerCase()}`}>
-      <Button className='flex items-center gap-3'>
-      <svg className='w-5 h-4' >
-       {icon}
-      </svg>
-      
-         {language}
+      <Button className="flex items-center gap-3">
+        <svg className="w-5 h-4">{icon}</svg>
+        {language}
       </Button>
     </Link>
   );
-} 
+}
 
 export default LanguageButton;
