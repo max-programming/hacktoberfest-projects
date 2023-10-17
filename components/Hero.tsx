2c84534684ed8a38bdb18bb26b66029382d47d48
massahoudou <9324@.Light>
2023-10-17 13:37:54 +0000

    remove console.log && formated the code with prettier

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -15,20 +15,17 @@ function Hero() {
   const formRef = useRef<HTMLFormElement | null>(null);
   const router = useRouter();
   const handleClear: MouseEventHandler<HTMLButtonElement> = () => {
-    if (formRef.current && !isSearchEmpty ) {
-
+    if (formRef.current && !isSearchEmpty) {
       formRef.current.reset();
       setErrorMessage(null);
     }
-
-  }
+  };
   const handleSubmit: FormEventHandler = e => {
     e.preventDefault();
     const formData = new FormData(e.target as HTMLFormElement);
     const search = (formData.get('search') as string).trim();
     // Check if the input is empty or contains only spaces
     if (search === '') {
-  
       setErrorMessage('Empty search terms invalid!');
       return;
     }
@@ -44,37 +41,37 @@ function Hero() {
           <h1 className="mb-5 text-2023-manga-3 text-5xl font-bold uppercase">
             Search your language
           </h1>
-          <form ref={formRef}
+          <form
+            ref={formRef}
             className="form-control w-full max-w-xs mx-auto items-center mt-10 mb-12"
             onSubmit={handleSubmit}
           >
             <div className="flex w-full">
-              <div className='flex relative'>
+              <div className="flex relative">
                 <input
                   type="text"
                   placeholder="Search for your language"
                   className="input input-bordered w-full text-neutral-100 border-2023-bavarian-gold-2 focus:outline-2023-bavarian-gold-2 max-w-xs rounded-tr-none rounded-br-none bg-transparent"
                   name="search"
-                  onChange={
-                    (e) =>  {
-                      setIsSearchEmpty(e.target.value.trim() === '') ;
-                      console.log(e.target.value.trim(),e.target.value.trim() === '' );
-                    }
-                  }
+                  onChange={e => {
+                    setIsSearchEmpty(e.target.value.trim() === '');
+                  }}
                 />
 
-                {!isSearchEmpty && 
-                <button onClick={handleClear} className='absolute right-0 top-0 bottom-0  p-2'>
-                  <ClearIcon />
-                </button>}
-
+                {!isSearchEmpty && (
+                  <button
+                    onClick={handleClear}
+                    className="absolute right-0 top-0 bottom-0  p-2"
+                  >
+                    <ClearIcon />
+                  </button>
+                )}
               </div>
               <button
                 type="submit"
                 className="group btn btn-square rounded-tl-none rounded-bl-none bg-transparent border-2023-manga-3 hover:bg-2023-manga-2 hover:text-2023-void-2 hover:border-2023-manga-2"
               >
                 <SearchIcon />
-
               </button>
             </div>
           </form>
@@ -133,13 +130,22 @@ const SearchIcon = () => (
   </svg>
 );
 const ClearIcon = () => {
-  return <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4 text-white ">
-    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
-  </svg>
-
-
-}
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="w-4 h-4 text-white "
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M6 18L18 6M6 6l12 12"
+      />
+    </svg>
+  );
+};
 
 export default Hero;
-
-
