9a5aceb6bca72af192ea31a949380560bb137f19
Usman S. (Max Programming) <51731966+max-programming@users.noreply.github.com>
2024-10-02 10:34:37 +0530

    Added nullable to another optional env variable

--- src/env.mjs
+++ src/env.mjs
@@ -11,7 +11,7 @@ export const env = createEnv({
     AUTH_GITHUB_TOKEN: z.string().optional()
   },
   client: {
-    NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME: z.string().optional(),
+    NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME: z.string().optional().nullable(),
     NEXT_PUBLIC_ANALYTICS_WEBSITE_ID: z.string().uuid().optional().nullable(),
   },
   experimental__runtimeEnv: {
