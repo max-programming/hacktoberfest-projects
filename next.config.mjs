5aba997aed083b6d4a0a753662d31caed8194a78
Usman S. <ui.sabuwala@gmail.com>
2025-08-03 12:55:04 +0000

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 import path from 'path';
+import { fileURLToPath } from 'url';
 import './src/env.mjs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 /**@type {import('next').NextConfig}*/
 const config = {
